c37a307a69b4d8f72572ba00aa9912f57038b618
Benjamin De Kosnik <bkoz@gnu.org>
2021-05-06 11:15:16 -0700

    automate common command line tasks

---
+++ scripts/generate-sort-forms-mmrl.sh
@@ -0,0 +1,14 @@
+mv $src/alpha60/src/a60-metadata-analyze.exe .;
+./a60-metadata-analyze.exe;
+
+mkdir svg ; mv *.svg ./svg/;
+mkdir svg.chord ; mv svg/*chord-graph* ./svg.chord/;
+mkdir svg.dim ; mv svg/*dimension-graph* ./svg.dim/;
+
+$src/alpha60/scripts/svg-dir-to-pngs-and-pdfs.sh ./svg
+$src/alpha60/scripts/svg-dir-to-pngs-and-pdfs.sh ./svg.chord
+$src/alpha60/scripts/svg-dir-to-pngs-and-pdfs.sh ./svg.dim
+
+$src/alpha60/scripts/sort-dirs-to-type.sh ./svg
+$src/alpha60/scripts/sort-dirs-to-type.sh ./svg.chord
+$src/alpha60/scripts/sort-dirs-to-type.sh ./svg.dim
